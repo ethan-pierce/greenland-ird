8f2183853b1567596fec6e0b0ba6b29f0421c25b
Ethan Pierce <ethan.pierce@colorado.edu>
2023-06-27 10:42:29 -0600

    Write clip2catchment processing routines

--- src/utils/clip_to_catchment.py
+++ src/utils/clip_to_catchment.py
@@ -1,6 +1,6 @@
 """Clips raster data to a vector shapefile.
 
-Given multiple input rasters and a vector shapefile of the catchment boundary, 
+Given multiple input rasters and a vector shapefile of the regional boundary, 
 this utility will clip each dataset to the catchment and resample the raster
 data to the same (x, y) size. The utility provides a single netCDF file as
 its output, with variables for each raster and the boundary shapefile.
@@ -9,6 +9,7 @@ Note: raster data is expected to be in netcdf format.
 Similarly, vector data is expected in geojson format.
 """
 
+import glob
 import xarray as xr
 import geopandas as gpd
 import rioxarray as rxr
@@ -102,20 +103,34 @@ class ClipToCatchment:
         ds.to_netcdf(output_path)
 
 def main():
-    """Runs the ClipToCatchment algorithm with user-specified inputs."""
-    Clip = ClipToCatchment(
-        'data/catchment-outlines/CW/eqip-sermia.geojson',
-        bed = 'data/ignore/BedMachineGreenland-v5.nc',
-        thickness = 'data/ignore/BedMachineGreenland-v5.nc',
-        vx = 'data/ignore/GRE_G0120_0000.nc',
-        vy = 'data/ignore/GRE_G0120_0000.nc'
-    )
+    """Runs the ClipToCatchment algorithm for all files in data/basin-outlines."""
+    shapefiles = glob.glob('data/basin-outlines/**/*.geojson')
 
-    shape = (100, 100)
-    ds = Clip.build_dataset(sampling = shape)
-    print(ds.variables)
-    print(ds.coords)
-    print(ds.dims)
+    var_names = {
+        'bed': 'bedrock_elevation',
+        'thickness': 'ice_thickness',
+        'vx': 'surface_velocity_x',
+        'vy': 'surface_velocity_y'
+    }
+
+    output_dir = 'data/basin-netcdfs/'
+
+    for basin in shapefiles:
+        basin_name = basin.split('/')[-1].replace('.geojson', '')
+
+        CC = ClipToCatchment(
+            basin,
+            bed = 'data/ignore/BedMachineGreenland-v5.nc',
+            thickness = 'data/ignore/BedMachineGreenland-v5.nc',
+            vx = 'data/ignore/GRE_G0120_0000.nc',
+            vy = 'data/ignore/GRE_G0120_0000.nc'
+        )
+
+        res_sample = CC.resample_to_resolution('bed', (100, 100))
+        CC.build_dataset(sampling = res_sample.shape)
+        CC.write_netcdf(output_dir + basin_name + '.nc', var_names)
+
+        break
 
 if __name__ == '__main__':
     main()
