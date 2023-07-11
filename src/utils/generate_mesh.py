f576d24fb8d0788ac659042e69ad41916ba709d5
Ethan Pierce <ethan.pierce@colorado.edu>
2023-07-11 16:12:08 -0600

    Implement VoronoiDelaunay graph structure

--- src/utils/generate_mesh.py
+++ src/utils/generate_mesh.py
@@ -12,24 +12,186 @@ import geopandas as gpd
 import triangle
 import shapely
 import matplotlib.pyplot as plt
+import xarray as xr
 
 class VoronoiDelaunay:
     """Constructs the Voronoi-Delaunay dual graph.
     
-    We need to supply the following:
-    * xy_of_node
-    * nodes_at_link
-    * links_at_patch
-    * xy_of_corner
-    * corners_at_face
-    * faces_at_cell
-    * node_at_cell
-    * nodes_at_face
+    We translate Landlab grid elements as follows:
+    * nodes: Delaunay vertices
+    * links: Delaunay edges
+    * patches: Delaunay triangles
+    * corners: Voronoi vertices
+    * faces: Voronoi edges
+    * cells: Voronoi polygons
     """
 
-    def __init__(self):
+    def __init__(self, delaunay: dict, voronoi: dict):
         """Initialize the constructor with dictionaries of Voronoi and Delaunay points."""
-        pass
+        self._delaunay = delaunay
+        self._voronoi = voronoi
+
+        self._mesh = xr.Dataset(
+            {
+                "node": xr.DataArray(
+                    data = np.arange(len(self._delaunay['vertices'])),
+                    coords = {
+                        "x_of_node": xr.DataArray(self._delaunay['vertices'][:, 0], dims = ("node",)),
+                        "y_of_node": xr.DataArray(self._delaunay['vertices'][:, 1], dims = ("node",)),
+                    },
+                    dims = ("node",),
+                ),
+                "corner": xr.DataArray(
+                    data = np.arange(len(self._voronoi['vertices'])),
+                    coords = {
+                        "x_of_corner": xr.DataArray(self._voronoi['vertices'][:, 0], dims = ("corner",)),
+                        "y_of_corner": xr.DataArray(self._voronoi['vertices'][:, 1], dims = ("corner",)),
+                    },
+                    dims = ("corner",),
+                ),
+            }
+        )
+
+        corners_at_cell = self._get_corners_at_cell()
+        self._mesh.update(
+            {
+                "nodes_at_link": xr.DataArray(
+                    self._delaunay['edges'], dims = ("link", "Two")
+                ),
+                "nodes_at_patch": xr.DataArray(
+                    self._delaunay['triangles'], dims = ("patch", "Three")
+                ),
+                "corners_at_face": xr.DataArray(
+                    self._voronoi['edges'], dims = ("face", "Two")
+                ),
+                "corners_at_cell": xr.DataArray(
+                    corners_at_cell, dims = ("cell", "max_corners_per_cell")
+                ), 
+                "n_corners_at_cell": xr.DataArray(
+                    [len(cell) for cell in corners_at_cell], dims = ("cell",)
+                ),
+                "nodes_at_face": xr.DataArray(
+                    self._get_nodes_at_face(), dims = ("face", "Two")
+                ),
+                "cell_at_node": xr.DataArray(
+                    np.arange(self.number_of_nodes), dims = ("node",)
+                )
+            }
+        )
+
+    def _get_corners_at_cell(self) -> np.ndarray:
+        """Construct an array of size (n_cells, max_corners_at_cell) from the Voronoi graph."""
+
+        # TODO can we do better than O(n^2) here?
+        corners = [[] for node in np.arange(self.number_of_nodes)]
+        max_corners_per_cell = 0
+
+        for cell in np.arange(self.number_of_nodes):
+            triangles = np.where(np.isin(self._delaunay['triangles'], cell))[0]
+            corners[cell] = triangles.tolist()
+
+            if len(triangles) > max_corners_per_cell:
+                max_corners_per_cell = len(triangles)
+
+        corners_at_cell = np.full((self.number_of_nodes, max_corners_per_cell), -1)
+        for cell in np.arange(self.number_of_nodes):
+            corners_at_cell[cell, :len(corners[cell])] = corners[cell]
+
+        return corners_at_cell
+
+    def _get_nodes_at_face(self) -> np.ndarray:
+        """Construct an array of size (n_faces, 2) from the Voronoi graph."""
+        nodes_at_face = np.full((len(self._voronoi['edges']), 2), -1)
+
+        for face in np.arange(len(self._voronoi['edges'])):
+            if face < len(self._delaunay['edges']):
+                nodes_at_face[face] = self._delaunay['edges'][face]
+            else:
+                pass
+
+        return nodes_at_face
+
+    @property
+    def perimiter_nodes(self):
+        return [
+            node for node in np.arange(self._delaunay['vertex_markers'].shape[0]) if
+            self._delaunay['vertex_markers'][node] == 1
+        ]
+
+    @property
+    def perimiter_links(self):
+        return [
+            link for link in np.arange(self._delaunay['edge_markers'].shape[0]) if
+            self._delaunay['edge_markers'][link] == 1
+        ]
+
+    @property
+    def number_of_nodes(self):
+        return self._mesh.dims["node"]
+
+    @property
+    def number_of_links(self):
+        return self._mesh.dims["link"]
+
+    @property
+    def number_of_patches(self):
+        return self._mesh.dims["patch"]
+
+    @property
+    def number_of_corners(self):
+        return self._mesh.dims["corner"]
+
+    @property
+    def number_of_faces(self):
+        return self._mesh.dims["face"]
+
+    @property
+    def number_of_cells(self):
+        return self._mesh.dims["cell"]
+
+    @property
+    def x_of_node(self):
+        return self._mesh["x_of_node"].values
+
+    @property
+    def y_of_node(self):
+        return self._mesh["y_of_node"].values
+
+    @property
+    def x_of_corner(self):
+        return self._mesh["x_of_corner"].values
+
+    @property
+    def y_of_corner(self):
+        return self._mesh["y_of_corner"].values
+
+    @property
+    def nodes_at_patch(self):
+        return self._mesh["nodes_at_patch"].values
+
+    @property
+    def nodes_at_link(self):
+        return self._mesh["nodes_at_link"].values
+
+    @property
+    def nodes_at_face(self):
+        return self._mesh["nodes_at_face"].values
+
+    @property
+    def corners_at_face(self):
+        return self._mesh["corners_at_face"].values
+
+    @property
+    def corners_at_cell(self):
+        return self._mesh["corners_at_cell"].values
+
+    @property
+    def n_corners_at_cell(self):
+        return self._mesh["n_corners_at_cell"].values
+
+    @property
+    def cell_at_node(self):
+        return self._mesh["cell_at_node"].values
 
 class MeshGenerator:
     """Uses Shewchuk's Triangle to generate a mesh over a specified domain."""
@@ -77,21 +239,31 @@ class MeshGenerator:
             'edges': edges
         }
 
-    def generate_grid():
-        """Generate a Landlab grid using the triangulated mesh."""
-        pass
+    def generate_graph(self):
+        """Generate a Landlab graph structure using the triangulated mesh."""
+        self._graph = VoronoiDelaunay(self.delaunay, self.voronoi)
+
+    def generate_grid(self):
+        """Generate a Landlab grid using the graph."""
+        self._grid = TestModelGrid(self._graph)
 
     def write_out():
         """Write out the generated geometry and/or Landlab grid."""
         pass
 
-    def plot(self, vertices, edges = None, bounds = None, subplots_kwargs: dict = {}):
+    def plot(
+        self, 
+        vertices, 
+        edges = None, 
+        bounds = None, 
+        subplots_kwargs: dict = {}
+    ):
         """Given a list of vertices with coordinates, plot the mesh."""
         fig, ax = plt.subplots(**subplots_kwargs)
 
         ax.scatter(vertices[:,0], vertices[:,1], color = 'tab:blue', s = 2)
 
-        if len(edges) > 0:
+        if edges is not None:
             for edge in edges:
                 a, b = edge
                 coords = [
@@ -122,21 +294,11 @@ def main():
 
     Mesh = MeshGenerator(path)
 
-    Mesh.triangulate(opts = 'pq30a100000Dez')
+    Mesh.triangulate(opts = 'pq30a1000000Dez')
 
-    Mesh.plot(Mesh.delaunay['vertices'], edges = Mesh.delaunay['edges'], subplots_kwargs = {'figsize': (16, 6)})
+    Mesh.generate_graph()
 
-    Mesh.plot(
-        Mesh.voronoi['vertices'], 
-        edges = Mesh.voronoi['edges'], 
-        bounds = [
-            np.min(Mesh.delaunay['vertices'][:,0]),
-            np.min(Mesh.delaunay['vertices'][:,1]),
-            np.max(Mesh.delaunay['vertices'][:,0]),
-            np.max(Mesh.delaunay['vertices'][:,1]),
-        ],
-        subplots_kwargs = {'figsize': (16, 6)}
-    )
+    Mesh.generate_grid()
 
 if __name__ == '__main__':
     main()
