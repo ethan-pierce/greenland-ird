c55fbb0ec3bc84057cf96944c8656f1bfd4a70ba
Ethan Pierce <ethan.pierce@colorado.edu>
2023-07-12 14:17:19 -0600

    Clean up mesh generator

--- src/utils/generate_mesh.py
+++ src/utils/generate_mesh.py
@@ -243,10 +243,6 @@ class MeshGenerator:
         """Generate a Landlab graph structure using the triangulated mesh."""
         self._graph = VoronoiDelaunay(self.delaunay, self.voronoi)
 
-    def generate_grid(self):
-        """Generate a Landlab grid using the graph."""
-        self._grid = TestModelGrid(self._graph)
-
     def write_out():
         """Write out the generated geometry and/or Landlab grid."""
         pass
@@ -280,6 +276,30 @@ class MeshGenerator:
 
         plt.show()
 
+def test_square_grid(n: int, opts: str = '', plot = True) -> dict:
+    ids = np.arange(n**2)
+    xs = np.ravel(np.array(np.meshgrid(np.arange(n), np.arange(n))[0]))
+    ys = np.ravel(np.array(np.meshgrid(np.arange(n), np.arange(n))[1]))
+
+    verts = [[xs[id], ys[id]] for id in ids]
+
+    tri = triangle.triangulate({'vertices': verts}, opts = opts)
+
+    if plot:
+        fig, ax = plt.subplots()
+        ax.scatter(tri['vertices'][:,0], tri['vertices'][:,1], color = 'tab:blue', s = 2)
+        for edge in tri['edges']:
+            a, b = edge
+            coords = [
+                [tri['vertices'][a][0], tri['vertices'][b][0], tri['vertices'][a][0]],
+                [tri['vertices'][a][1], tri['vertices'][b][1], tri['vertices'][a][1]]
+            ]
+
+            ax.plot(coords[0], coords[1], color = 'tab:orange', linewidth = 1)
+        plt.show()
+
+    return tri
+
 def main():
     parser = argparse.ArgumentParser(description = 'Mesh generation utility.')
     parser.add_argument(
@@ -294,11 +314,18 @@ def main():
 
     Mesh = MeshGenerator(path)
 
-    Mesh.triangulate(opts = 'pq30a1000000Dez')
+    Mesh.triangulate(opts = 'pq30a1000000Dze')
 
     Mesh.generate_graph()
 
-    Mesh.generate_grid()
+    print(Mesh.delaunay.keys())
+    print(Mesh.voronoi.keys())
+
+    # Mesh.plot(
+    #     Mesh.delaunay['vertices'],
+    #     Mesh.delaunay['edges'],
+    #     subplots_kwargs = {'figsize': (16, 6)}
+    # )
 
 if __name__ == '__main__':
     main()
