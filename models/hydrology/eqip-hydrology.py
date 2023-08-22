2b7551b04af4401704feda8a0208035be826a91c
Ethan Pierce <ethan.pierce@colorado.edu>
2023-08-22 11:54:46 -0600

    Identify gridding issues

--- models/hydrology/eqip-hydrology.py
+++ models/hydrology/eqip-hydrology.py
@@ -1,39 +1,119 @@
 """Models the subglacial drainage system at Eqip Sermia, CW Greenland."""
 
 import numpy as np
+import xarray as xr
+import rioxarray as rxr
+import jax
 import matplotlib.pyplot as plt
+import pickle
 
+from basis.utils.grid_loader import GridLoader
 from basis.components.jax_conduit_network import ConduitNetwork
-from landlab import TriangleMeshGrid
 
-class HydrologicModel:
-    """Set up boundary conditions, model subglacial hydrology, and plot results."""
+# Step 1: Pre-process ice thickness, bedrock elevation, and ice velocity
+# path = "/home/egp/repos/greenland-ird/data/basin-outlines/CW/eqip-sermia.geojson"
+# gl = GridLoader(path, quality = 30, max_area = 400**2)
+# print(
+#     "Generated grid for: ",
+#     path.split("/")[-1].replace("-", " ").replace(".geojson", "").capitalize(),
+# )
+# print(
+#     "Grid consists of "
+#     + str(gl.grid.number_of_nodes)
+#     + " nodes and "
+#     + str(gl.grid.number_of_links)
+#     + " links."
+# )
+# im = plt.scatter(gl.grid.node_x, gl.grid.node_y, s = 2)
+# plt.colorbar(im)
+# plt.show()
 
-    def __init__(self, shapefile: str, mesh_opts: str = 'pqDevjz'):
-        """Initialize the model with a path to a shapefile."""
-        self.grid = TriangleMeshGrid.from_shapefile(shapefile, triangle_opts = mesh_opts)
+# bedmachine = "/home/egp/repos/greenland-ird/data/ignore/BedMachineGreenland-v5.nc"
+# gl.add_field(
+#     bedmachine,
+#     "thickness",
+#     "ice_thickness",
+#     crs="epsg:3413",
+#     neighbors=100,
+#     no_data=-9999.0,
+# )
+# print("Added ice thickness to grid nodes.")
+# im = plt.scatter(gl.grid.node_x, gl.grid.node_y, c = gl.grid.at_node['ice_thickness'], s = 2)
+# plt.colorbar(im)
+# plt.show()
 
+# gl.add_field(
+#     bedmachine,
+#     "bed",
+#     "bedrock_elevation",
+#     crs="epsg:3413",
+#     neighbors=100,
+#     no_data=-9999.0,
+# )
+# print("Added bedrock elevation to grid nodes.")
+# im = plt.scatter(gl.grid.node_x, gl.grid.node_y, c = gl.grid.at_node['bedrock_elevation'], s = 2)
+# plt.colorbar(im)
+# plt.show()
 
-def main():
-    """Plot the drainage system evolution over the model run."""
-    model = HydrologicModel(
-        '/home/egp/repos/greenland-ird/data/basin-outlines/CW/eqip-sermia.geojson',
-        mesh_opts = 'pa160000q30Devjz'
-    )
+# velocity = "/home/egp/repos/greenland-ird/data/ignore/GRE_G0120_0000.nc"
+# gl.add_field(velocity, "v", "surface_velocity", crs="epsg:3413", neighbors=100)
+# print("Added surface velocity to grid nodes.")
+# im = plt.scatter(gl.grid.node_x, gl.grid.node_y, c = gl.grid.at_node['surface_velocity'], s = 2)
+# plt.colorbar(im)
+# plt.show()
 
-    fig = model.grid.plot_nodes_and_links(
-        subplots_args = {'figsize': (12, 4)},
-        nodes_args = {'s': 5, 'color': 'tab:orange'},
-        links_args = {'lw': 1, 'linestyle': ':', 'color': 'tab:blue'}
-    )
-    plt.show()
-    
-    print('Grid size: ', model.grid.number_of_links, ' links')
-    print('Grid size: ', model.grid.number_of_nodes, ' nodes')
-    print('Mean element area: ', np.mean(
-        model.grid.area_of_patch[model.grid.area_of_patch > 0]
-    ), ' m^2.')
-    print('Mean link length: ', np.mean(model.grid.length_of_link), ' m.')
+# velocity_links = gl.grid.map_mean_of_link_nodes_to_link("surface_velocity")
+# gl.grid.add_field("ice_sliding_velocity", velocity_links * 0.6, at="link")
 
-if __name__ == '__main__':
-    main()
+# # Step 2: Estimate meltwater input
+# lapse = 5e-3
+# t0 = 2
+# z0 = 400
+# kh = 24
+# rho = 917
+# L = 3.34e5
+
+# air_temperature = t0 - lapse * (
+#     gl.grid.at_node["ice_thickness"][:] + gl.grid.at_node["bedrock_elevation"][:] - z0
+# )
+
+# melt = air_temperature * kh / (rho * L)
+
+# gl.grid.add_field("meltwater_input", melt, at="node")
+# print("Added meltwater input to grid nodes.")
+
+# # Step 3: Set up the ConduitNetwork fields
+# base_effective_pressure = gl.grid.at_node["ice_thickness"] * 917 * 9.81
+# gl.grid.add_field(
+#     "effective_pressure",
+#     gl.grid.map_mean_of_link_nodes_to_link(base_effective_pressure),
+#     at="link",
+# )
+
+# base_hydraulic_gradient = 1000 * 9.81 * gl.grid.map_mean_of_link_nodes_to_link(
+#     "bedrock_elevation"
+# ) + gl.grid.calc_grad_at_link(base_effective_pressure)
+# gl.grid.add_field("hydraulic_gradient", base_hydraulic_gradient, at="link")
+
+# gl.grid.add_field("conduit_area", np.full(gl.grid.number_of_links, 0.1), at="link")
+# gl.grid.add_field("water_flux", gl.grid.map_mean_of_link_nodes_to_link('meltwater_input'), at = "link")
+
+# gl.grid.save('/home/egp/repos/greenland-ird/models/hydrology/eqip-sermia.grid', clobber = True)
+# ##########################################
+
+with open('/home/egp/repos/greenland-ird/models/hydrology/eqip-sermia.grid', 'rb') as pf:
+    grid = pickle.load(pf)
+
+print(grid.number_of_nodes)
+print(np.count_nonzero(~np.isnan(grid.at_node['ice_thickness'])))
+
+# model = ConduitNetwork(grid)
+# print("Established conduit network.")
+
+# melt = model._calc_melt_opening()
+# gap = model._calc_gap_opening()
+# opening = melt + gap
+
+# im = plt.scatter(grid.node_x, grid.node_y, s = 2)
+# plt.colorbar(im)
+# plt.show()
