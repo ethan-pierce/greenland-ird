cb1bf4e8ad1f489b408c4dbd4496ed50ddd9abb2
Ethan Pierce <ethan.pierce@colorado.edu>
2023-11-10 15:52:05 -0700

    Explore results

--- igm/model-runs/eqip-sermia/plot_result.py
+++ igm/model-runs/eqip-sermia/plot_result.py
@@ -3,16 +3,19 @@ from netCDF4 import Dataset
 import matplotlib.pyplot as plt
 
 ds = Dataset('optimize.nc')
+print(ds.variables.keys())
 
 im = plt.imshow(ds.variables['thk'][-1])
 plt.colorbar(im)
+plt.title('Ice thickness (m)')
 plt.show()
 
 im = plt.imshow(ds.variables['velsurf_mag'][-1])
 plt.colorbar(im)
+plt.title('Surface velocity (m a$^{-1}$)')
 plt.show()
 
 im = plt.imshow(ds.variables['slidingco'][-1])
 plt.colorbar(im)
+plt.title('Sliding coefficient')
 plt.show()
-
