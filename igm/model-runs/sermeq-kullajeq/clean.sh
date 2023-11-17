76029040989072015236d4239386ae4e66718072
Ethan Pierce <ethan.pierce@colorado.edu>
2023-11-17 12:38:05 -0700

    Add all results

--- igm/model-runs/sermeq-kullajeq/clean.sh
+++ igm/model-runs/sermeq-kullajeq/clean.sh
@@ -4,3 +4,11 @@ rm geology-optimized.nc
 rm convergence.png
 rm rms_std.dat
 rm costs.dat
+rm params_saved.json
+rm optimize.nc
+rm geology-optimized.nc
+rm convergence.png
+rm rms_std.dat
+rm costs.dat
+rm params_saved.json
+rm optimize.nc
