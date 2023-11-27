7bf31ede195f7b9a8666ec20f4db9e85fa9d2964
Ethan Pierce <ethan.pierce@colorado.edu>
2023-11-27 13:48:38 -0700

    Try to fix local IGM build

--- igm/model-runs/akullersuup-sermia/clean.sh
+++ igm/model-runs/akullersuup-sermia/clean.sh
@@ -38,3 +38,13 @@ rm geology-optimized.nc
 rm convergence.png
 rm rms_std.dat
 rm costs.dat
+rm params_saved.json
+rm optimize.nc
+rm params_saved.json
+rm optimize.nc
+rm params_saved.json
+rm optimize.nc
+rm params_saved.json
+rm optimize.nc
+rm params_saved.json
+rm optimize.nc
