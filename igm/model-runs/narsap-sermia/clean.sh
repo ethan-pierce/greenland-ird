e14b95b1081d5629be3736a4f7ead1fb501f5a41
Ethan Pierce <ethan.pierce@colorado.edu>
2023-11-30 09:30:37 -0700

    No thk optimize

--- igm/model-runs/narsap-sermia/clean.sh
+++ igm/model-runs/narsap-sermia/clean.sh
@@ -40,3 +40,9 @@ rm geology-optimized.nc
 rm convergence.png
 rm rms_std.dat
 rm costs.dat
+rm params_saved.json
+rm optimize.nc
+rm geology-optimized.nc
+rm convergence.png
+rm rms_std.dat
+rm costs.dat
--- igm/model-runs/sermeq-kullajeq/clean.sh
+++ igm/model-runs/sermeq-kullajeq/clean.sh
@@ -30,3 +30,5 @@ rm geology-optimized.nc
 rm convergence.png
 rm rms_std.dat
 rm costs.dat
+rm params_saved.json
+rm optimize.nc
